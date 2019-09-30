e5ad271a33c307f4828adf596c11fe3639abc3d6
Jasmine Kim <jas89922@gmail.com>
2019-09-30 00:24:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,8 +31,8 @@ end
 def using_delete(array, string)
 array.delete
 end
-def using_delete_at(array, Integer)
-  array.delete_at(Integer)
+def using_delete_at(array, integer)
+  array.delete_at(integer)
 end
 
 
