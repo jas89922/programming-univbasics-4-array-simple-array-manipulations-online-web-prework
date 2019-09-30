7a36bfb7c5dba93a18d5b86a4f5f248869674775
Jasmine Kim <jas89922@gmail.com>
2019-09-30 00:08:55 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,38 @@
+def using_push
+  array.push
+end
+def using_unshift
+  array.unshift
+end
+def using_pop
+
+end
+def pop_with_args
+  
+end
+def using_shift
+  
+end
+def shift_with_args
+  
+end
+def using_concat
+  
+end
+def using_insert
+  
+end
+def using_uniq
+  
+end
+def using_flatten
+  
+end
+def using_delete
+
+end
+def using_delete_at
+  
+end
+
+
