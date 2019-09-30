0c9c7125bdf68c33ad145ec391d2ec6a90ff0b17
Jasmine Kim <jas89922@gmail.com>
2019-09-30 00:30:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,13 +5,13 @@ def using_unshift(array, string)
   array.unshift(string)
 end
 def using_pop(array)
-array.pop(array)
+array.pop
 end
 def pop_with_args(array)
   array.pop(2)
   end
 def using_shift(array)
-  array.shift(0)
+  array.shift
 end
 def shift_with_args(array)
   array.shift(2)
@@ -20,7 +20,7 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 def using_insert(array)
-  array.insert(4)
+  array.insert
 end
 def using_uniq(array)
   array.uniq
@@ -29,7 +29,7 @@ def using_flatten
   array.flatten
 end
 def using_delete(array, string)
-array.delete
+  array.delete
 end
 def using_delete_at(array, integer)
   array.delete_at(integer)
