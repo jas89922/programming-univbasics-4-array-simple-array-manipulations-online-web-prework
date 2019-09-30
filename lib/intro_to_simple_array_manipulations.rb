c173e15b9d27f78dcd50659e291941184a1f43dc
Jasmine Kim <jas89922@gmail.com>
2019-09-30 00:32:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,13 +19,13 @@ end
 def using_concat(array1, array2)
   array1.concat(array2)
 end
-def using_insert(array)
-  array.insert
+def using_insert(array, element)
+  array.insert[4]
 end
 def using_uniq(array)
   array.uniq
 end
-def using_flatten
+def using_flatten(array)
   array.flatten
 end
 def using_delete(array, string)
